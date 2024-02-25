9a76abe5f3f625907dffe6fa6fd3592d5e8b7794
eunhak <akfmzh1207@naver.com>
2024-02-26 00:08:39 +0900

    feat : login api

--- src/api/apiAxios.ts
+++ src/api/apiAxios.ts
@@ -3,6 +3,8 @@ import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
 
+const token = cookies.get('token');
+
 const apiAxios = axios.create({
   baseURL: import.meta.env.VITE_APP_SERVICE_URL,
   headers: {
@@ -10,3 +12,34 @@ const apiAxios = axios.create({
   },
   timeout: 5000,
 });
+
+apiAxios.interceptors.request.use(config => {
+  const { url, headers } = config;
+
+  console.log('url', url);
+  console.log('headers', headers);
+
+  return config;
+});
+
+apiAxios.interceptors.response.use(config => {
+  const { headers } = config;
+
+  console.log('headers', headers);
+
+  return config;
+});
+
+//   response => response,
+//   error => {
+//     const statusCode = error.response?.status;
+//     if (statusCode === 401) {
+//       error.response.statusText = 'Unauthorized';
+//       error.response.status = 401;
+//       window.location.href = '/';
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+export default apiAxios;
--- src/api/login.ts
+++ src/api/login.ts
@@ -1,8 +1,21 @@
 import axios from 'axios';
-import apiAxios from '../apiAxios';
+import apiAxios from './apiAxios';
+
+import { Cookies } from 'react-cookie';
+const cookies = new Cookies();
+
+
+export const setCookie = (name :string, value : string, option?:any) => {
+  return cookies.set(name, value, { ...option });
+};
+export const getCookie = (name : string) => {
+  return cookies.get(name);
+};
+
+
 
 export const login = async (email: string, password: string) => {
-  const response = await apiAxios
+  const response = await axios
     .post('v2/admin/login', {
       loginId: 'string',
       password: 'string',
