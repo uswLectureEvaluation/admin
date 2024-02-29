67d0af226d3ad6f7e83ff5e287fc784500ded7ef
eunhak <akfmzh1207@naver.com>
2024-02-29 18:28:40 +0900

    getall api

--- src/api/getall.ts
+++ src/api/getall.ts
@@ -1,8 +1,9 @@
+import { ReportData } from '../types/all';
 import apiAxios from './apiAxios';
-// import AxiosResponse from 'axios';
+
 export const getall = async () => {
   await apiAxios
-    .get('v2/admin/reported-posts')
+    .get<ReportData>('v2/admin/reported-posts')
     .then(r => {
       r.data;
     })
