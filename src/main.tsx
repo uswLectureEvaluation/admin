04a4c803605f8d1ce4380e681c11be4bc3cabf4a
eun-hak <akfmzh1207@suwon.ac.kr>
2024-03-02 01:05:41 +0900

    setting react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,20 @@ import App from './App.tsx';
 import './index.css';
 // import { BrowserRouter } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie';
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+let queryClient = new QueryClient(); //캐시와 훅을 쓸수있게 정의
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <CookiesProvider>
       <App />
+      <ReactQueryDevtools />
     </CookiesProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
