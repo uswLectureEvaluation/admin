4562aa6368244921f1fcad77f2c26201b5b50284
Chung-il Jung <jungchungil7@gmail.com>
2024-03-18 15:48:50 +0900

    feat(admin): eslint react plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,19 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-refresh'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
 
       { allowConstantExport: true },
     ],
-    "react/jsx-pascal-case": "warn",
+    'react/jsx-pascal-case': 'warn',
   },
 };
